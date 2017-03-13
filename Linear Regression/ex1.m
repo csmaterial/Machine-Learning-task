655a1689975bcd58226b6dabc1de96d628572f55
csmaterial <csmaterial@users.noreply.github.com>
2017-03-13 14:30:46 +0800

    Update ex1.m

--- Linear Regression/ex1.m
+++ Linear Regression/ex1.m
@@ -18,6 +18,7 @@
 %
 %  For this exercise, you will not need to change any code in this file,
 %  or any other files other than those mentioned above.
+
 %
 % x refers to the population size in 10,000s
 % y refers to the profit in $10,000s
@@ -46,6 +47,35 @@ m = length(y); % number of training examples
 % Note: You have to complete the code in plotData.m
 plotData(X, y);
 
+function plotData(x, y)
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data 
+ylabel('Profit in $10,000s'); % Set the y?axis label
+xlabel('Population of City in 10,000s'); % Set the x?axis label
+
+
+% ============================================================
+
+end
+
+
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
@@ -61,8 +91,64 @@ alpha = 0.01;
 
 % compute and display initial cost
 J = computeCost(X, y, theta)
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+
+J = sum((X*theta - y).^2) / (2 * m);
+
+
+% =========================================================================
+
+end
+
 
 % run gradient descent
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    
+   temp0 = theta(1,:) - sum((X*theta-y).*X(:,1))/m*alpha;
+   temp1 = theta(2,:) - sum((X*theta-y).*X(:,2))/m*alpha;
+    theta(1,:) = temp0;
+    theta(2,:) = temp1;
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
 % print theta to screen
